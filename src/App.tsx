d1e845624e2eb7c8d5864b8d60b9a05dbadfeccb
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2025-01-29 17:48:12 -0800

    fixed local

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <p>Testing!!!</p>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
