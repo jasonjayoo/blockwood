4e607dca21b67c505b2254df543e494d06cdf33e
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2025-01-31 12:47:12 -0800

    pushing up current changes

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+ import { cva } from "class-variance-authority"
+ 
+ 
+ const buttonStyles = cva(["hover:bg-secondary-hover", "transition-"]) 
+ 
+ 
+ export function Button(){
+    return <button />
+ }
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,3 +1,17 @@
+import { Menu } from "lucide-react"
+import logo from "../assets/blockwood.png"
+
 export function PageHeader() {
-    return null;
+    return <div className="flex gap-10 lg:gap-20 justify-between">
+        <div className="flex gap-4 items-center flex-shrink-0">
+            <button>
+                <Menu />
+            </button>
+            <a href="/">
+                <img src={logo} className="h-14 w-40"  />
+            </a>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
 }
