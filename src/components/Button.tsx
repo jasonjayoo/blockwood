1810770cf2b4c4c960340ea814225ef70f2adafe
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2025-01-31 18:24:48 -0800

    added new logo - header completed - nav bar completed - continuing work

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,16 @@
  import { cva } from "class-variance-authority"
  
  
- const buttonStyles = cva(["hover:bg-secondary-hover", "transition-"]) 
+ const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+    variants: {
+        size: {
+            default: [],
+            icon: ["rounded-full", "w-10", "h-10", "flex", "item-center", "justify-center"],
+        }
+    }
+ }) 
  
+const classes = buttonStyles({ size: "icon"})
  
  export function Button(){
     return <button />
