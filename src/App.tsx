423233a8241422305ec6482b433056abb6c126b3
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2025-02-04 15:08:00 -0800

    blockwood finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 
+import { useState } from "react"
+import { CategoryPills } from "./components/CategoryPills"
+import { categories, videos } from "./data/home"
 import { PageHeader } from "./layouts/PageHeader"
+import { VideoGridItem } from "./components/VideoGridItem"
+import { Sidebar } from "./layouts/Sidebar"
+import { SidebarProvider } from "./contexts/SidebarContext"
 
 export default function App() {
+
+  const [selectedCategory, setSelectedCategory] = useState(categories[0])
   return (
-    <div className="max-h-screen flex flex-col">
-      <PageHeader />
-      <div>2</div>
-    </div>
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
+            </div>
+            <div className="grid [grid-template-columns:repeat(auto-fill,_minmax(300px,_1fr))] gap-5">
+              {videos.map(video => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
   )
 }
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,17 +1,79 @@
-import { Menu } from "lucide-react"
-import logo from "../assets/blockwood.png"
+
+import { useState, useEffect } from "react"
+import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react"
+import logo from "../assets/blockwood-transparent2.png"
+import { Button } from "../components/Button"
+import { useSidebarContext } from "../contexts/SidebarContext"
 
 export function PageHeader() {
-    return <div className="flex gap-10 lg:gap-20 justify-between">
-        <div className="flex gap-4 items-center flex-shrink-0">
-            <button>
+    const [showFullWidthMobile, setShowFullWidthMobile] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 768) {
+                setShowFullWidthMobile(false)
+            }
+        }
+        window.addEventListener("resize", handleResize)
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
+
+    return <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+    <PageHeaderFirstSection hidden={showFullWidthMobile} />
+        <form className={`gap-4 flex-grow justify-center ${showFullWidthMobile ? "flex" : "hidden md:flex"}`}>
+            {showFullWidthMobile && (
+                <Button onClick={() => setShowFullWidthMobile(false)} type="button" size="icon" variant="ghost" className="flex-shrink-0">
+                    <ArrowLeft />
+                </Button> 
+            )}  
+            <div className="flex flex-grow  max-w-[600px]">
+                <input type="search" placeholder="Search" className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none" />
+                    <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+                        <Search />
+                    </Button>
+            </div>
+            <Button type="button" size="icon" className="flex-shrink-0">
+                <Mic />
+            </Button>
+        </form>
+        <div className={`flex-shrink-0 md:gap-2 ${showFullWidthMobile ? "hidden" : "flex"}`}>
+            <Button onClick={() => setShowFullWidthMobile(true)} size="icon" variant="ghost" className="md:hidden">
+                <Search />
+            </Button>
+            <Button size="icon" variant="ghost" className="md:hidden">
+                <Mic />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <Upload />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <Bell />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <User />
+            </Button>
+        </div>
+    </div>
+}
+
+type PageHeaderFirstSectionProps = {
+    hidden?: boolean
+}   
+
+export function PageHeaderFirstSection({ hidden = false }: PageHeaderFirstSectionProps) {
+
+    const { toggle } = useSidebarContext()
+    
+    return <div 
+            className={`gap-4 items-center flex-shrink-0 ${hidden ? "hidden" : "flex"}`}>
+            <Button onClick={toggle} variant="ghost" size="icon" >
                 <Menu />
-            </button>
+            </Button>
             <a href="/">
-                <img src={logo} className="h-14 w-40"  />
+                {/* <img src={logo} className="h-6 w-45 sm:h-10 sm:w-auto"/> */}
+                <img src={logo} className="h-6" />
             </a>
         </div>
-        <div></div>
-        <div></div>
-    </div>
 }
