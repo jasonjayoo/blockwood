22b9fc6674d344b64874740138895bebcee4ab38
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2025-02-04 16:56:40 -0800

    Adjusting header icons for mobile view

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@
         },
         size: {
             default: ["rounded", "p-2"],
-            icon: ["rounded-full", "w-10", "h-10", "flex", "item-center", "justify-center", "p-2"],
+            icon: ["rounded-full", "w-9", "h-9", "md:w-10", "md:h-10", "flex", "items-center", "justify-center", "p-2"],
         },
     },
     defaultVariants: {
